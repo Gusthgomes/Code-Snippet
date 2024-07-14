24a1322a1fb94c1cf83163046cc8667b35dc534d
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-07-14 16:19:29 -0300

    refactor: changing the file directory

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+import logoPng from "/public/logo.png";
+import { UserButton } from "@clerk/nextjs";
+
+export function Header(p: {}) {
+  return (
+    <header className="bg-main-700 p-8 flex justify-between items-center">
+      <Link href="/">
+        <Image src={logoPng} alt="Logo" className="w-48" />
+      </Link>
+      <div className="flex ml-">
+        <UserButton />
+      </div>
+    </header>
+  );
+}
