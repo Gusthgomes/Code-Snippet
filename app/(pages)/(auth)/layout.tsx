fd3f3a764aacc2210fba56eca147dbc0392cde49
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-07-14 16:20:28 -0300

    feat: adding header component on layout page

--- app/(pages)/(auth)/layout.tsx
+++ app/(pages)/(auth)/layout.tsx
@@ -11,7 +11,7 @@ export default function AuthLayout({
       <div className="flex-1 flex flex-col space-y-4 items-center bg-main-700 justify-center shadow-2xl rounded-bl-[8rem]">
         <Image src={logoAuthPng} alt="Logo" />
         <div className="text-2xl text-white">
-          The one place for all your code snippets
+          O único lugar para todos os seus trechos de código
         </div>
       </div>
     </div>
