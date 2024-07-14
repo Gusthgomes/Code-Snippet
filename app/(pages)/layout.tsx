8e4ba98945e97675755fe73c003d6d105e8554cd
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-07-14 16:05:12 -0300

    feat: created header component and layouts

---
+++ app/(pages)/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Snipia",
+  description: "The one place for all your code snippets",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        variables: { colorPrimary: "#d44700" },
+        elements: { card: "bg-main-700 shadow-none" },
+      }}
+    >
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
